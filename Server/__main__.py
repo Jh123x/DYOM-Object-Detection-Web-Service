af7e6e8729c898695c8321c1c27421a4f9e6bd90
Wen Junhua <wenjh1998@gmail.com>
2021-04-16 14:41:08 +0800

    Initial commit for object detection web tool

--- Server/__main__.py
+++ Server/__main__.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask.helpers import flash, send_file, send_from_directory
+from werkzeug.utils import secure_filename
+from functions.object_detect import draw_bounding_boxes
+import os
+
+
+# Constants
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+UPLOAD_FOLDER = './uploads'
+
 
 # Create the application
 app = Flask(__name__)
+app.secret_key = os.urandom(64).hex()
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 # Create the routes
@@ -11,16 +23,49 @@ def welcome_page():
     return render_template('index.html')
 
 
-@app.route('/upload')
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_img_page():
     """The page for the user to upload their images"""
-    pass
 
+    # Redirect to mainpage if the method is get
+    if request.method == 'GET':
+        return redirect('/')
 
-@app.route('/result', methods=['POST'])
-def results():
-    """The page where the results will be shown"""
-    pass
+    # Check if any file is uploaded
+    if 'file' not in request.files:
+        flash('File uploaded is invalid','Error')
+        return redirect('/')
+
+    # Get the file
+    file = request.files['file']
+
+    # Check if the files uploaded is not empty
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+
+    # Check if the extension of the file is correct
+    for ext in ALLOWED_EXTENSIONS:
+        if ext in file.filename:
+            curr_ext = ext
+            break
+    else:
+        flash(f"File extension is not allowed. We only allow {', '.join(ALLOWED_EXTENSIONS)}.")
+        return redirect('/')
+
+    # Save the file to a path
+    print(f'Valid file received: {file}')
+
+    # Get the filepath
+    filename = secure_filename(f'temp.{curr_ext}')
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    # Save the file at path
+    file.save(filepath)
+
+    # Pass the file_path to the object detection code and let the user download the file
+    response_filepath = draw_bounding_boxes(filepath, curr_ext)
+    return send_file(response_filepath)
 
 
 # If the file is run as main
